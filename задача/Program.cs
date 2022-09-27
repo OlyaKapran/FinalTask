58038e0abb329e58ef86e71f78376b761ec0ed0c
Olga <olyakapran@yandex.by>
2022-09-27 14:38:36 +0300

    second commit

---
+++ задача/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace задача
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
