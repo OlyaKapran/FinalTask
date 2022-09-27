1a274698611503550c33014467203f01249f071f
Olga <olyakapran@yandex.by>
2022-09-27 15:02:37 +0300

    third commit

--- задача/Program.cs
+++ задача/Program.cs
@@ -6,7 +6,26 @@ namespace задача
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string[] arr1 = new string[5] {"Minsk", "hello", "11", "-.", "3"};
+            string[] arr2 = new string[arr1.Length];
+            int count = 0;
+            for (int i = 0; i < arr1.Length; i++)
+            {
+                if(arr1[i].Length <= 3)
+                {
+                    arr2[count] = arr1[i];
+                    count++;
+                }
+            }
+            void PrintArray(string[] array)
+            {
+                for (int i = 0; i < array.Length; i++)
+                {
+                    Console.Write($"{array[i]} ");
+                }
+                Console.WriteLine();
+            }
+            PrintArray(arr2);
         }
     }
 }
